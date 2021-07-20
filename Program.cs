304c7a2b00f1a4e6be270b7efa4318ef4d85f232
Mulang Su <sumulang@gmail.com>
2021-07-20 10:49:10 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Album
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
