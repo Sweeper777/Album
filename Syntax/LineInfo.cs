5055b2e20574d0c307b893d25bb976fdcc37b4c3
Mulang Su <sumulang@gmail.com>
2021-07-21 11:37:49 +0800

    added factory methods for LineInfo

--- Syntax/LineInfo.cs
+++ Syntax/LineInfo.cs
@@ -11,5 +11,26 @@ namespace Album.Syntax {
             this.stringValue = stringValue;
             this.intValue = intValue;
         }
+
+        public static LineInfo OfType(LineType type) {
+            if (type == LineType.Push ||
+                type == LineType.Label ||
+                type == LineType.Branch) {
+                throw new ArgumentException(
+                    "type must not be a type of line that takes arguments!",
+                    nameof(type)
+                    );
+            }
+            return new LineInfo(type, null, null);
+        }
+
+        public static LineInfo Push(int number)
+            => new LineInfo(LineType.Push, null, number);
+
+        public static LineInfo Label(string name)
+            => new LineInfo(LineType.Label, name, null);
+
+        public static LineInfo Branch(string label)
+            => new LineInfo(LineType.Branch, label, null);
     }
 }
