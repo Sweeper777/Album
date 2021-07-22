4ec27fb04cd7112a7dad00848ef9c448d3d4d033
Mulang Su <sumulang@gmail.com>
2021-07-22 22:15:50 +0800

    made SongManifest implement ISongManifest

--- Syntax/SongManifest.cs
+++ Syntax/SongManifest.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Collections.Generic;
 
 namespace Album.Syntax {
-    public class SongManifest {
+    public class SongManifest : ISongManifest{
 
         [JsonProperty("SongNames")]
         public Dictionary<string, LineType> SongNames { get; }
@@ -15,6 +15,10 @@ namespace Album.Syntax {
         [JsonProperty("SpecialSongs")]
         public SpecialSongs SpecialSongs { get; }
 
+        public SpecialSongInfo PushXSyntax => SpecialSongs.PushX;
+
+        public SpecialSongInfo BranchSyntax => SpecialSongs.Branch;
+
         public SongManifest(Dictionary<string, LineType> songNames,
                             Dictionary<string, int> specialPushes,
                             SpecialSongs specialSongs) {
@@ -22,6 +26,37 @@ namespace Album.Syntax {
             SpecialPushes = specialPushes;
             SpecialSongs = specialSongs;
         }
+
+        public static SongManifest? FromStream(Stream stream) {
+            var reader = new StreamReader(stream);
+            var serializer = new JsonSerializer();
+            var jsonTextReader = new JsonTextReader(reader);
+            return serializer.Deserialize<SongManifest>(jsonTextReader);
+        }
+
+        public static SongManifest? FromFile(string path) {
+            using var fileStream = File.OpenRead(path);
+            return FromStream(fileStream);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(SongNames, SpecialPushes, SpecialSongs);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is SongManifest manifest &&
+                   EqualityComparer<Dictionary<string, LineType>>.Default.Equals(SongNames, manifest.SongNames) &&
+                   EqualityComparer<Dictionary<string, int>>.Default.Equals(SpecialPushes, manifest.SpecialPushes) &&
+                   EqualityComparer<SpecialSongs>.Default.Equals(SpecialSongs, manifest.SpecialSongs);
+        }
+
+        public LineType GetFixedLineType(string songName)
+            => SongNames.GetValueOrDefault(songName);
+
+        public int? GetFixedPushAmount(string songName)
+            => SpecialPushes.TryGetValue(songName, out int value) ? value : null;
     }
 
     public class SpecialSongs {
@@ -35,6 +70,18 @@ namespace Album.Syntax {
             PushX = pushX;
             Branch = branch;
         }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is SpecialSongs songs &&
+                   EqualityComparer<SpecialSongInfo>.Default.Equals(PushX, songs.PushX) &&
+                   EqualityComparer<SpecialSongInfo>.Default.Equals(Branch, songs.Branch);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PushX, Branch);
+        }
     }
 
     public class SpecialSongInfo {
@@ -47,6 +94,18 @@ namespace Album.Syntax {
         public SpecialSongInfo(string startWith, string endWith) {
             StartWith = startWith;
             EndWith = endWith;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is SpecialSongInfo info &&
+                   StartWith == info.StartWith &&
+                   EndWith == info.EndWith;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StartWith, EndWith);
         }
     }
 }
