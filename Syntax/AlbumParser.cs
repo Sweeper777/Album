e767d6d9e029023a4be2aee969b5bcda79587c57
Mulang Su <sumulang@gmail.com>
2021-07-22 22:17:27 +0800

    Create AlbumParser.cs

---
+++ Syntax/AlbumParser.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Album.Syntax {
+    public class AlbumParser: IDisposable {
+        private const string PLAYLIST_CREATOR_PREFIX = "playlist created by ";
+
+        private bool disposedValue;
+        private StreamReader reader;
+        private int currentLine = 0;
+
+        public ISongManifest SongManifest { get; set; }
+        public IList<CompilerOutput> Outputs { get; set; } = new List<CompilerOutput>();
+
+        public AlbumParser(ISongManifest manifest, Stream stream) {
+            SongManifest = manifest;
+            this.reader = new StreamReader(stream);
+        }
+
+        public AlbumParser(SongManifest manifest, string path) 
+            : this(manifest, File.OpenRead(path)) {
+
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    reader.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
