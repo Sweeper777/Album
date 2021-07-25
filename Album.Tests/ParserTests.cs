aa5d4a0c682338e02704f9a73b46da95b718e7ae
Mulang Su <sumulang@gmail.com>
2021-07-25 11:57:52 +0800

    added CannotFindPlaylistCreatorDecl test

--- Album.Tests/ParserTests.cs
+++ Album.Tests/ParserTests.cs
@@ -18,5 +18,17 @@ namespace Album.Tests {
                 CollectionAssert.Contains(lines, LineInfo.OriginalSong("original song"));
             });
         }
+
+        [TestCase("playlist       created     by    Sweeper")]
+        [TestCase("Foo\nBar\nBaz\n")]
+        [TestCase("This is a playlist created by Sweeper")]
+        public void CannotFindPlaylistCreatorDecl(string code) {
+            string codeWithLabelAdded = code + "\noriginal song, by Sweeper";
+            using var parser = new AlbumParser(new DummySongManifest(), codeWithLabelAdded);
+            var lines = parser.Parse();
+            CollectionAssert.IsNotEmpty(parser.Outputs);
+            Assert.AreEqual(CompilerMessage.NoPlaylistCreatorDeclFound, parser.Outputs[0].Message);
+            CollectionAssert.IsSubsetOf(lines, new List<LineInfo> { LineInfo.OfType(LineType.Comment) });
+        }
     }
 }
