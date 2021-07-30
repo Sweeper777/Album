0fb804d965d4e97764649bcf6fed80c271387653
Mulang Su <sumulang@gmail.com>
2021-07-30 18:06:13 +0800

    added some fields for SemanticAnalyser

--- Album/Semantics/SemanticAnalyser.cs
+++ Album/Semantics/SemanticAnalyser.cs
@@ -1,9 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using Album.Syntax;
 
 namespace Album.Semantics {
     public class SemanticAnalyser {
+        public IList<CompilerOutput> Outputs { get; } = new List<CompilerOutput>();
+
+        private IEnumerable<LineInfo> lines;
+
+        private HashSet<LineInfo> originalSongLines = new();
+        private HashSet<LineInfo> branchLines = new();
+
+        public SemanticAnalyser(IEnumerable<LineInfo> lines) {
+            this.lines = lines;
+        }
 
     }
 }
