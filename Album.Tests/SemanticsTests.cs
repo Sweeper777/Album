e3efca176a95800eb344f5760fd009a532b213e1
Mulang Su <sumulang@gmail.com>
2021-07-31 12:32:39 +0800

    tested FindsUnknownOriginalSongs

--- Album.Tests/SemanticsTests.cs
+++ Album.Tests/SemanticsTests.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 using Album.Semantics;
-using System.Linq;
 using static Album.Syntax.LineInfo;
 using static Album.Syntax.LineType;
 
@@ -21,5 +20,25 @@ namespace Album.Tests {
             analyser.Analyse();
             CollectionAssert.IsEmpty(analyser.Outputs);
         }
+
+        [Test]
+        public void FindsUnknownOriginalSongs() {
+            SemanticAnalyser analyser = new(new[] {
+                Push(1, 2),
+                OfType(Dup, 3),
+                OfType(OutputChar, 5),
+                Branch("foo", 6),
+                Branch("bar", 7)
+            });
+            analyser.Analyse();
+            CollectionAssert.AreEquivalent(
+                new[] { 
+                    new CompilerOutput(CompilerMessage.UnknownOriginalSong, CompilerOutputType.Error, 6),
+                    new CompilerOutput(CompilerMessage.UnknownOriginalSong, CompilerOutputType.Error, 7)
+                },
+                analyser.Outputs
+            );
+        }
+
     }
 }
