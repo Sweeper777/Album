ec52720abb520d8075864bbcb539460f016f8b8f
Mulang Su <sumulang@gmail.com>
2021-08-02 10:50:28 +0800

    rewrote the CecilCodeGenerator

--- Album/CodeGen/Cecil/CecilCodeGenerator.cs
+++ Album/CodeGen/Cecil/CecilCodeGenerator.cs
@@ -4,72 +4,77 @@ using Mono.Cecil.Cil;
 using Mono.Cecil;
 using static Album.CodeGen.Cecil.CecilCodeGenerationStrategies;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Album.CodeGen.Cecil
 {
-    public class CecilCodeGenerator : CodeGenerator, IMethodReferenceProvider
+    public class CecilCodeGenerator : CodeGenerator
     {
-        public AssemblyDefinition GeneratedAssembly { get; }
-        public ModuleDefinition GeneratedModule => GeneratedAssembly.MainModule;
+        public AssemblyDefinition? GeneratedAssembly { get; private set; }
+        public ModuleDefinition? GeneratedModule => GeneratedAssembly?.MainModule;
 
-        private ILProcessor il;
+        private ILProcessor? il;
 
-        public CecilCodeGenerator(IEnumerable<LineInfo> lines) : base(lines)
-        {
+        public CecilCodeGenerator(IEnumerable<LineInfo> lines) : base(lines) {
+        }
+
+        private Dictionary<LineType, CecilCodeGenerationStrategy>? strategies;
+        private MethodReferenceProvider? methodReferences;
+
+        protected override ICodeGenerationStrategy? GetCodeGenerationStrategyForSong(LineType type)
+            => strategies?.GetValueOrDefault(type);
+
+        protected override void WillGenerateLines() {
             GeneratedAssembly = AssemblyDefinition.CreateAssembly(
                 new AssemblyNameDefinition("AlbumPlaylist", new Version(1, 0, 0, 0)), "AlbumPlaylist", ModuleKind.Console);
 
             var programType = new TypeDefinition("AlbumPlaylist", "Program",
-                Mono.Cecil.TypeAttributes.Class | Mono.Cecil.TypeAttributes.Public, GeneratedModule.TypeSystem.Object);
+                Mono.Cecil.TypeAttributes.Class | Mono.Cecil.TypeAttributes.Public, GeneratedModule?.TypeSystem.Object);
 
-            GeneratedModule.Types.Add(programType);
-
-            consoleWrite = GeneratedModule.ImportReference(typeof(Console).GetMethod("WriteLine", new[] { typeof(string) }));
-            linkedListAddLast = GeneratedModule.ImportReference(typeof(LinkedList<int>).GetMethod("AddLast", new[] { typeof(int) }));
-            consoleRead = GeneratedModule.ImportReference(typeof(Console).GetMethod("Read", new Type[] { }));
-            linkedListNodeValue = GeneratedModule.ImportReference(typeof(LinkedListNode<int>).GetProperty("Value")?.GetGetMethod());
-            linkedListLast = GeneratedModule.ImportReference(typeof(LinkedList<int>).GetProperty("Last")?.GetGetMethod());
-            linkedListRemoveLast = GeneratedModule.ImportReference(typeof(LinkedList<int>).GetMethod("RemoveLast", new Type[] { }));
+            GeneratedModule?.Types.Add(programType);
 
             var mainMethod = new MethodDefinition("Main",
-                Mono.Cecil.MethodAttributes.Public | Mono.Cecil.MethodAttributes.Static, GeneratedModule.TypeSystem.Void);
+                Mono.Cecil.MethodAttributes.Public | Mono.Cecil.MethodAttributes.Static, GeneratedModule?.TypeSystem.Void);
 
-            var stackVar = new VariableDefinition(GeneratedModule.TypeSystem.Int32);
+            var stackVar = new VariableDefinition(GeneratedModule?.TypeSystem.Int32);
             mainMethod.Body.Variables.Add(stackVar);
             programType.Methods.Add(mainMethod);
 
             var argsParameter = new ParameterDefinition("args",
-                Mono.Cecil.ParameterAttributes.None, GeneratedModule.ImportReference(typeof(string[])));
+                Mono.Cecil.ParameterAttributes.None, GeneratedModule?.ImportReference(typeof(string[])));
 
             mainMethod.Parameters.Add(argsParameter);
 
             il = mainMethod.Body.GetILProcessor();
-            il.Emit(OpCodes.Newobj, GeneratedModule.ImportReference(typeof(LinkedList<int>).GetConstructor(new Type[] {})));
+            il.Emit(OpCodes.Newobj, GeneratedModule?.ImportReference(typeof(LinkedList<int>).GetConstructor(new Type[] {})));
 
             GeneratedAssembly.EntryPoint = mainMethod;
 
-            strategies = new() {
-                { LineType.Input, new Input(this, il) }
+            InitialiseMethodReferences();
+            InitialiseCodeGenStrategies(methodReferences, il);
+        }
+
+        [MemberNotNull(nameof(methodReferences))]
+        private void InitialiseMethodReferences() {
+            methodReferences = new() {
+                ConsoleWrite = GeneratedModule?.ImportReference(typeof(Console).GetMethod("WriteLine", new[] { typeof(string) })),
+                LinkedListAddLast = GeneratedModule?.ImportReference(typeof(LinkedList<int>).GetMethod("AddLast", new[] { typeof(int) })),
+                ConsoleRead = GeneratedModule?.ImportReference(typeof(Console).GetMethod("Read", new Type[] { })),
+                LinkedListNodeValue = GeneratedModule?.ImportReference(typeof(LinkedListNode<int>).GetProperty("Value")?.GetGetMethod()),
+                LinkedListLast = GeneratedModule?.ImportReference(typeof(LinkedList<int>).GetProperty("Last")?.GetGetMethod()),
+                LinkedListRemoveLast = GeneratedModule?.ImportReference(typeof(LinkedList<int>).GetMethod("RemoveLast", new Type[] { })),
             };
         }
 
-        private readonly Dictionary<LineType, CecilCodeGenerationStrategy> strategies;
-        private readonly MethodReference linkedListAddLast;
-        private readonly MethodReference linkedListRemoveLast;
-        private readonly MethodReference linkedListLast;
-        private readonly MethodReference linkedListNodeValue;
-        private readonly MethodReference consoleRead;
-        private readonly MethodReference consoleWrite;
-
-        MethodReference IMethodReferenceProvider.LinkedListAddLast => linkedListAddLast;
-
-        MethodReference IMethodReferenceProvider.LinkedListRemoveLast => linkedListRemoveLast;
-        MethodReference IMethodReferenceProvider.LinkedListLast => linkedListLast;
-        MethodReference IMethodReferenceProvider.LinkedListNodeValue => linkedListNodeValue;
-        MethodReference IMethodReferenceProvider.ConsoleRead => consoleRead;
-        MethodReference IMethodReferenceProvider.ConsoleWrite => consoleWrite;
-
-        protected override ICodeGenerationStrategy? GetCodeGenerationStrategyForSong(LineType type)
-            => strategies.GetValueOrDefault(type);
+        private void InitialiseCodeGenStrategies(MethodReferenceProvider methodReferences, ILProcessor il) {
+            strategies = new() {
+                { LineType.Input, new Input(methodReferences, il) }
+            };
+        }
+        protected override void DidGenerateLines()
+        {
+            il?.Emit(OpCodes.Pop);
+            il?.Emit(OpCodes.Ret);
+        }
     }
 }
