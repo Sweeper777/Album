20ea31cc31fca27817f84e481b0fbeafb241b5fa
Mulang Su <sumulang@gmail.com>
2021-08-03 10:31:45 +0800

    implemented codegen for pop

---
+++ Album/CodeGen/Cecil/CecilPop.cs
@@ -0,0 +1,23 @@
+using Album.Syntax;
+using Mono.Cecil.Cil;
+
+namespace Album.CodeGen.Cecil
+{
+    internal partial class CecilCodeGenerationStrategies {
+        public class Pop : CecilCodeGenerationStrategy
+        {
+            public Pop(MethodReferenceProvider methods, ILProcessor ilProcessor) 
+                : base(methods, ilProcessor) {
+            }
+
+            public override void GenerateCodeForSong(LineInfo line)
+            {
+                ILProcessor.Emit(OpCodes.Dup);
+                ILProcessor.Emit(OpCodes.Call, methods.LinkedListRemoveLast);
+            }
+
+            public override bool SupportsLineType(LineType type)
+                => type == LineType.Pop;
+        }
+    }
+}
