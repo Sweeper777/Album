4085d9c8b54dc24ba6fe1415acf94b206f6c2bcb
Mulang Su <sumulang@gmail.com>
2021-08-03 10:31:30 +0800

    implemented codegen for branch and original song

---
+++ Album/CodeGen/Cecil/CecilBranch.cs
@@ -0,0 +1,54 @@
+using Album.Syntax;
+using System.Collections.Generic;
+using Mono.Cecil.Cil;
+
+namespace Album.CodeGen.Cecil
+{
+    internal partial class CecilCodeGenerationStrategies {
+        public class BranchOriginalSong : CecilCodeGenerationStrategy
+        {
+            private Dictionary<string, Instruction> originalSongs = new();
+
+            public BranchOriginalSong(MethodReferenceProvider methods, ILProcessor ilProcessor) 
+                : base(methods, ilProcessor) {
+            }
+
+            public override void GenerateCodeForSong(LineInfo line)
+            {
+                if (line.IsBranch(out string? originalSong)) {
+                    GenerateBranch(originalSong);
+                } else if (line.IsOriginalSong(out originalSong)) {
+                    GenerateOriginalSong(originalSong);
+                }
+            }
+
+            private void GenerateBranch(string originalSong) {
+                Instruction? target = originalSongs.GetValueOrDefault(originalSong);
+                if (target is null) {
+                    target = ILProcessor.Create(OpCodes.Nop);
+                    originalSongs.Add(originalSong, target);
+                }
+                ILProcessor.Emit(OpCodes.Dup);
+                ILProcessor.Emit(OpCodes.Callvirt, methods.LinkedListLast);
+                ILProcessor.Emit(OpCodes.Callvirt, methods.LinkedListNodeValue);
+                ILProcessor.Emit(OpCodes.Stloc_0);
+                ILProcessor.Emit(OpCodes.Dup);
+                ILProcessor.Emit(OpCodes.Callvirt, methods.LinkedListRemoveLast);
+                ILProcessor.Emit(OpCodes.Ldloc_0);
+                ILProcessor.Emit(OpCodes.Brtrue, target);
+            }
+
+            private void GenerateOriginalSong(string name) {
+                Instruction? existing = originalSongs.GetValueOrDefault(name);
+                if (existing is null) {
+                    existing = ILProcessor.Create(OpCodes.Nop);
+                    originalSongs.Add(name, existing);
+                }
+                ILProcessor.Append(existing);
+            }
+
+            public override bool SupportsLineType(LineType type)
+                => type == LineType.Branch || type == LineType.OriginalSong;
+        }
+    }
+}
