d29d2bbfae5be0aec9a83908a93d02979d17d0ac
Mulang Su <sumulang@gmail.com>
2021-08-06 17:59:01 +0800

    added ProgramProducesCorrectOutput test case

--- Album.Tests/CecilCodeGeneratorTests.cs
+++ Album.Tests/CecilCodeGeneratorTests.cs
@@ -8,6 +8,18 @@ namespace Album.Tests {
     [Timeout(1000)]
     public class CecilCodeGeneratorTests {
         
+
+        [TestCase("Album.Tests.50plus1000minus7.album", "1043")]
+        public void ProgramProducesCorrectOutput(string programResourceName, string expectedOutput) {
+            using var stream = typeof(CecilCodeGeneratorTests).Assembly.GetManifestResourceStream(programResourceName);
+            Assert.IsNotNull(stream);
+            CecilCodeGenerator codegen = new();
+            AlbumCompiler compiler = new(codegen);
+            compiler.Compile(stream!);
+            Assert.NotNull(codegen.GeneratedAssembly);
+            Assert.AreEqual(expectedOutput, RunAssembly(codegen.GeneratedAssembly!).Trim());
+        }
+
         private string RunAssembly(AssemblyDefinition asmDef) {
             asmDef.Write("TestOutput.exe");
             using var proc = new Process
