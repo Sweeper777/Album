f6ea020434465195b0e9980d912ec6337994ac79
Mulang Su <sumulang@gmail.com>
2021-08-12 10:35:35 +0800

    implemented IOptimisationContext

--- Album/Semantics/CodeOptimiser.cs
+++ Album/Semantics/CodeOptimiser.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using Album.Syntax;
 
 namespace Album.Semantics {
-    public class CodeOptimiser {
+    public class CodeOptimiser : IOptimisationContext {
         private LinkedList<LineInfo> seenLines = new();
 
         public IEnumerable<OptimisationRule> Rules { get; set; } = Enumerable.Empty<OptimisationRule>();
+
+        int IOptimisationContext.CurrentLineCount => seenLines.Count;
+
+        IEnumerable<LineInfo> IOptimisationContext.PreviousLines {
+            get {
+                var lastNode = seenLines.Last;
+                while (lastNode != null) {
+                    yield return lastNode.Value;
+                    lastNode = lastNode.Previous;
+                }
+            }
+        }
 
         public IList<LineInfo> Optimise(IEnumerable<LineInfo> lines) {
             seenLines.Clear();
