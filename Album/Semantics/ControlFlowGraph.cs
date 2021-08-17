9222c4cf0735da322a58c771c3a9ecd98f4b33b3
Mulang Su <sumulang@gmail.com>
2021-08-17 11:16:30 +0800

    added BasicBlockBuilder

--- Album/Semantics/ControlFlowGraph.cs
+++ Album/Semantics/ControlFlowGraph.cs
@@ -17,5 +17,23 @@ namespace Album.Semantics {
             SourceCode = sourceCode;
         }
 
+
+        public class BasicBlockBuilder {
+            private ControlFlowGraph controlFlowGraph;
+            private int startIndex;
+            private int endIndexExclusive;
+
+            private bool added = false;
+
+            public BasicBlockBuilder(ControlFlowGraph controlFlowGraph, int start)
+            {
+                if (start < 0 || start > controlFlowGraph.SourceCode.Count) {
+                    throw new IndexOutOfRangeException("Start index of basic block is out of range!");
+                }
+                this.controlFlowGraph = controlFlowGraph;
+                startIndex = start;
+                endIndexExclusive = start;
+            }
+
     }
 }
