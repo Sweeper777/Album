5b9b3f213f2f6e5fde035097617c2bffbf77cde5
Mulang Su <sumulang@gmail.com>
2021-08-17 11:15:00 +0800

    added ControlFlowGraph property to BasicBlock

--- Album/Semantics/BasicBlock.cs
+++ Album/Semantics/BasicBlock.cs
@@ -5,10 +5,23 @@ using System;
 
 namespace Album.Semantics {
     public class BasicBlock {
+        public BasicBlock(int startIndex, int endIndexExclusive, ControlFlowGraph controlFlowGraph)
+        {
+            if (startIndex < 0 || startIndex > controlFlowGraph.SourceCode.Count ||
+                endIndexExclusive < 0 || endIndexExclusive > controlFlowGraph.SourceCode.Count) {
+                throw new IndexOutOfRangeException("Indices for the basic block are out of range!");
+            }
+
+            StartIndex = startIndex;
+            EndIndexExclusive = endIndexExclusive;
+            ControlFlowGraph = controlFlowGraph;
+        }
 
         public int StartIndex { get; }
 
         public int EndIndexExclusive { get; }
+
+        public ControlFlowGraph ControlFlowGraph { get; }
 
     }
 }
