cd7d5bf913b645f653270725006b01d9b03df45b
Mulang Su <sumulang@gmail.com>
2021-08-17 11:14:20 +0800

    Create BasicBlock.cs

---
+++ Album/Semantics/BasicBlock.cs
@@ -0,0 +1,14 @@
+using Album.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Album.Semantics {
+    public class BasicBlock {
+
+        public int StartIndex { get; }
+
+        public int EndIndexExclusive { get; }
+
+    }
+}
