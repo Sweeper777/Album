1f5d77638f238e778c52b710d01d6dbdf9b6242a
Mulang Su <sumulang@gmail.com>
2021-08-20 17:16:43 +0800

    added GeneratesSimpleCFG test

--- Album.Tests/SemanticsTests.cs
+++ Album.Tests/SemanticsTests.cs
@@ -2,6 +2,8 @@ using NUnit.Framework;
 using Album.Semantics;
 using static Album.Syntax.LineInfo;
 using static Album.Syntax.LineType;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Album.Tests {
     [Timeout(1000)]
@@ -75,5 +77,26 @@ namespace Album.Tests {
                 analyser.Outputs
             );
         }
+
+        [Test]
+        public void GeneratesSimpleCFG() {
+            SemanticAnalyser analyser = new();
+            analyser.Analyse(new[] {
+                Push(1), Push(2), Push(3), OfType(OutputInt)
+            });
+            var cfg = analyser.CFG;
+            Assert.NotNull(cfg);
+            Assert.AreEqual(1, cfg!.BasicBlocks.Count);
+            Assert.Multiple(() => {
+                Assert.AreEqual(0, cfg!.BasicBlocks[0].StartIndex);
+                Assert.AreEqual(4, cfg!.BasicBlocks[0].EndIndexExclusive);
+                Assert.That(cfg.Successors, Is.All
+                    .Matches<KeyValuePair<BasicBlock, HashSet<BasicBlock>>>(
+                        x => !x.Value.Any()
+                    )
+                );
+            });
+        }
+
     }
 }
