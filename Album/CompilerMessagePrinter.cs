f12a78476549633e2333c103c2f41ffd533c9aa7
Mulang Su <sumulang@gmail.com>
2021-08-25 10:25:51 +0800

    get the appropriate localisation file from culture

--- Album/CompilerMessagePrinter.cs
+++ Album/CompilerMessagePrinter.cs
@@ -8,6 +8,31 @@ namespace Album {
     public interface ICompilerMessagePrinter {
         void Print(CompilerOutput output);
     }
+    public class LocalisedCompilerMessagePrinter : ICompilerMessagePrinter {
+        private LocalisedCompilerMessages messages;
+
+        public LocalisedCompilerMessagePrinter(CultureInfo culture) {
+            
+
+            using var stream = GetResourceStreamFromCulture(culture) ?? 
+                                GetResourceStreamFromCulture(new CultureInfo("en"));
+            if (stream == null) {
+                throw new IOException("No suitable localisation file found!");
+            }
+            var reader = new StreamReader(stream);
+            var serializer = new JsonSerializer();
+            var jsonTextReader = new JsonTextReader(reader);
+            messages = serializer.Deserialize<LocalisedCompilerMessages>(jsonTextReader) ??
+                        throw new JsonSerializationException($"Invalid localisation file for {culture}");
+        }
+
+        private static Stream? GetResourceStreamFromCulture(CultureInfo culture) {
+            if (culture.Name == "") return null;
+            return typeof(LocalisedCompilerMessagePrinter).Assembly.GetManifestResourceStream($"Album.{culture.Name}.json")
+                ?? GetResourceStreamFromCulture(culture.Parent);
+        }
+
+    }
 
     public class LocalisedCompilerMessages {
         public LocalisedCompilerMessages(
