b47e8b2bc9117061d79f5a44c2868b810b4b5e8c
Mulang Su <sumulang@gmail.com>
2021-08-25 10:24:39 +0800

    added LocalisedCompilerMessages

--- Album/CompilerMessagePrinter.cs
+++ Album/CompilerMessagePrinter.cs
@@ -8,4 +8,26 @@ namespace Album {
     public interface ICompilerMessagePrinter {
         void Print(CompilerOutput output);
     }
+
+    public class LocalisedCompilerMessages {
+        public LocalisedCompilerMessages(
+            string format, 
+            Dictionary<CompilerOutputType, string> localisedTypeNames, 
+            Dictionary<CompilerMessage, string> localisedMessages
+            ) {
+            Format = format;
+            LocalisedTypeNames = localisedTypeNames;
+            LocalisedMessages = localisedMessages;
+        }
+
+        [JsonProperty("Format")]
+        public string Format { get; }
+
+        [JsonProperty("TypeNames")]
+        public Dictionary<CompilerOutputType, string> LocalisedTypeNames { get; }
+
+        [JsonProperty("Messages")]
+        public Dictionary<CompilerMessage, string> LocalisedMessages { get; }
+    }
+
 }
