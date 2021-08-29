4b9cf574e2fa70dcf2d95d85493d49719a074c91
Mulang Su <sumulang@gmail.com>
2021-08-29 10:25:50 +0800

    tried to dynamically load the assembly instead

--- Album.Benchmarks/Program.cs
+++ Album.Benchmarks/Program.cs
@@ -5,6 +5,8 @@ using BenchmarkDotNet.Running;
 using Album.CodeGen.Cecil;
 using System.IO;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Album.Benchmarks
 {
@@ -37,53 +39,19 @@ namespace Album.Benchmarks
     }
 
     public class ExecutionTime {
-        private static long RunAssembly(string executableName) {
-            using var proc = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "dotnet",
-                    Arguments = executableName,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true,
-                    WorkingDirectory = ""
-                }
-            };
-            proc.Start();
-            proc.WaitForExit();
-            return proc.UserProcessorTime.Ticks;
+
+        public MethodInfo optimised, unoptimised;
+
         }
 
-        public static long RunWithOptimisation() => RunAssembly("optimised.dll");
 
-        public static long RunWithoutOptimisation() => RunAssembly("unoptimised.dll");
 
-        public static long RunControl() => RunAssembly("Beer.dll");
+        // public static Task<long> RunControl() => RunAssembly("Beer.dll");
     }
     class Program
     {
         static void Main(string[] args)
         {
-            // var summary = BenchmarkRunner.Run<CompilationTime>();
-
-            var codegen = new CecilCodeGenerator();
-            var compiler = new AlbumCompiler(codegen) { EnableOptimisation = true };
-            compiler.Compile(typeof(CompilationTime).Assembly.GetManifestResourceStream("Album.Benchmarks.99_bottles_of_beer.album"));
-            codegen.GeneratedAssembly.Write("optimised.dll");
-            compiler.EnableOptimisation = false;
-            compiler.Compile(typeof(CompilationTime).Assembly.GetManifestResourceStream("Album.Benchmarks.99_bottles_of_beer.album"));
-            codegen.GeneratedAssembly.Write("unoptimised.dll");
-            string runtimeConfigContents;
-            using (var stream = new StreamReader(typeof(AlbumCompiler).Assembly.GetManifestResourceStream("Album.runtimeconfig_template.json")!)) {
-                runtimeConfigContents = stream.ReadToEnd();
-            }
-            File.WriteAllText("optimised.runtimeconfig.json", runtimeConfigContents);
-            File.WriteAllText("unoptimised.runtimeconfig.json", runtimeConfigContents);
-
-            System.Console.WriteLine($"Control: {ExecutionTime.RunControl()} Ticks");
-            System.Console.WriteLine($"Optimisation: {ExecutionTime.RunWithOptimisation()} Ticks");
-            System.Console.WriteLine($"No Optimisation: {ExecutionTime.RunWithoutOptimisation()} Ticks");
         }
     }
 }
