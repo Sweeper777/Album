a3cbb463c74b09bbbc415daae28fbd6c786ec5b6
Mulang Su <sumulang@gmail.com>
2022-02-17 22:25:47 +0000

    simplified the requirements for CodeGenerator

--- Album/CodeGen/CodeGenerator.cs
+++ Album/CodeGen/CodeGenerator.cs
@@ -8,25 +8,6 @@ namespace Album.CodeGen
     public abstract class CodeGenerator {
         [DisallowNull]
         protected IEnumerable<LineInfo>? lines;
-        protected abstract ICodeGenerationStrategy? GetCodeGenerationStrategyForSong(LineType type);
-        protected virtual void WillGenerateLines() { }
-        protected virtual void DidGenerateLines() { }
-
-        public void GenerateCode(IEnumerable<LineInfo> lines) {
-            this.lines = lines;
-            WillGenerateLines();
-            foreach (var line in lines) {
-                var strategy = GetCodeGenerationStrategyForSong(line.Type);
-                if (strategy is null) {
-                    continue;
-                }
-                if (strategy.SupportsLineType(line.Type)) {
-                    strategy.GenerateCodeForSong(line);
-                } else {
-                    throw new InvalidOperationException($"Code Generation Strategy {strategy} Does Not Support Line Type {line.Type}!");
-                }
-            }
-            DidGenerateLines();
-        }
+        public abstract void GenerateCode(IEnumerable<LineInfo> lines);
     }
 }
