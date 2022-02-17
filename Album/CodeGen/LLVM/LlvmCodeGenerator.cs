70cd5eaf1a81d54d07d7f2db13bdb15f770b659b
Mulang Su <sumulang@gmail.com>
2022-02-17 22:26:48 +0000

    Create LlvmCodeGenerator.cs

---
+++ Album/CodeGen/LLVM/LlvmCodeGenerator.cs
@@ -0,0 +1,19 @@
+using Album.Syntax;
+using LLVMSharp;
+using System;
+using System.Linq;
+using Album.Semantics;
+using static LLVMSharp.LLVM;
+using System.Collections.Generic;
+
+namespace Album.CodeGen.LLVM {
+    public class LlvmCodeGenerator : CodeGenerator
+    {
+
+        private LLVMModuleRef generatedModule;
+        private LLVMBuilderRef builder;
+        private LLVMValueRef mainFunction;
+        private LLVMValueRef putcharFunction;
+        private ControlFlowGraph? cfg;
+    }
+}
