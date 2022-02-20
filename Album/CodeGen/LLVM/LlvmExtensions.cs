a04b210c6a936877d43c287a8aa02fd114b65eef
Mulang Su <sumulang@gmail.com>
2022-02-20 08:40:12 +0000

    added ToLlvmValue extension for longs

--- Album/CodeGen/LLVM/LlvmExtensions.cs
+++ Album/CodeGen/LLVM/LlvmExtensions.cs
@@ -6,6 +6,8 @@ namespace Album.CodeGen.LLVM {
     static class LlvmExtensions {
         public static LLVMValueRef ToLlvmValue(this int x)
             => ConstInt(Int32Type(), (ulong)x, true);
+        public static LLVMValueRef ToLlvmValue(this long x)
+            => ConstInt(Int64Type(), (ulong)x, true);
         
     }
 }
