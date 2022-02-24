b79064763bfc0663671cf828b609b9c72e9cb0cc
Mulang Su <sumulang@gmail.com>
2022-02-24 22:04:15 +0000

    added AddFunction LLVM extension method

--- Album/CodeGen/LLVM/LlvmExtensions.cs
+++ Album/CodeGen/LLVM/LlvmExtensions.cs
@@ -12,5 +12,18 @@ namespace Album.CodeGen.LLVM {
         public static LLVMTypeRef Pointer(this LLVMTypeRef type)
             => PointerType(type, 0);
 
+        public static LLVMValueRef AddFunction(
+            this LLVMModuleRef module,
+            string name,
+            LLVMTypeRef returnType,
+            params LLVMTypeRef[] parameterTypes
+        ) {
+            var func = AddFunction(module, name, FunctionType(
+                returnType, parameterTypes, false
+            ));
+            SetLinkage(func, LLVMLinkage.LLVMExternalLinkage);
+            return func;
+        }
+
     }
 }
