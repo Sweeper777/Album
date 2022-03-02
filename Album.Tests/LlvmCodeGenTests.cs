30dd91c82cdebeaf993d818d68925896a920151a
Mulang Su <sumulang@gmail.com>
2022-03-02 22:14:01 +0000

    added LlvmCodeGenTests

---
+++ Album.Tests/LlvmCodeGenTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using Album.CodeGen.LLVM;
+using System.IO;
+using System.Diagnostics;
+
+namespace Album.Tests {
+    [Timeout(10000)]
+    public class LlvmCodeGenTests {
+        
+
+    }
+}
